4fd9de9bf54e0edd3862a76d35ca61d6f9866b0e
wil3789 <cwb74@georgetown.edu>
2023-04-18 11:39:18 -0400

    in-class update
    able to pull features from different html files rather than just text when clicking different locations.

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -7,25 +7,25 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
-    #return render_template("home.html")
+  #  return "Welcome Home, let's talk about your living situation here."
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
-    #return render_template("about.html")
+   # return "You ain't nothin at all"
+    return render_template("about.html")
 
 @home_routes.route("/hello")
 def hello_world():
-    print("HELLO...")
+    print("Get off my lawn")
 
     url_params = dict(request.args)
     print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
 
     # compile message using specified "name" url parameter or a default value:
-    name = url_params.get("name") or "World"
+    name = url_params.get("You a real POS") or "You a real POS"
     message = f"Hello, {name}!"
 
-    return message
-    #return render_template("hello.html", message=message)
+   # return message
+    return render_template("hello.html", message=message)
